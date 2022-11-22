3c4095fdbe978c8f6e8c42213c342886c3bb81a4
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-11-22 16:14:20 +0200

    add ChakraProvider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import {App} from 'components/App';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/slimmom'>
-      <App/>
+      <ChakraProvider>
+        <App/>
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
