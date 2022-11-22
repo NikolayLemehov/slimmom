e0b47a6ada57845712f8de8f362e8339ddeefc71
Victor Grossu <weekgrossu@gmail.com>
2022-11-22 23:46:11 +0200

    update side bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,6 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
-import {
-  Box,
-  Divider,
-  Heading,
-  IconButton,
-  Link,
-  List,
-  ListItem,
-  Stack,
-  Text,
-} from '@chakra-ui/react';
+import { Divider, IconButton, Link, Stack, Text } from '@chakra-ui/react';
+import RightSideBar from 'components/RightSideBar/RightSideBar';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo/logo.png';
 
@@ -71,60 +62,7 @@ const Header = () => {
           background="transparent"
         />
       </Stack>
-      <Box
-        bgColor="#F0F1F3"
-        minW={{ xs: '768px', md: '517px' }}
-        maxW={{ lg: '517px' }}
-        py={{ xs: '40px', md: '80px', lg: '166px' }}
-        display="flex"
-        flexDir={{ xs: 'column', md: 'row', lg: 'column' }}
-        gap={{ xs: '40px', md: '97px', lg: '60px' }}
-        justifyContent="center"
-        alignItems={{ xs: 'center', md: 'baseline', lg: 'center' }}
-        ml={{ lg: 'auto' }}
-      >
-        <Box w="288px">
-          <Heading fontSize="14px" textAlign="center" mb="20px" color="#212121">
-            Summary for 06/20/2020
-          </Heading>
-          <List color="#9B9FAA">
-            <ListItem display="flex" justifyContent="space-between">
-              <Text>Left</Text>
-              <Text>000 kcal</Text>
-            </ListItem>
-            <ListItem display="flex" justifyContent="space-between">
-              <Text>Consumed</Text>
-              <Text>000 kcal</Text>
-            </ListItem>
-            <ListItem display="flex" justifyContent="space-between">
-              <Text>Daily rate</Text>
-              <Text>000 kcal</Text>
-            </ListItem>
-            <ListItem display="flex" justifyContent="space-between">
-              <Text>n% of normal</Text>
-              <Text>000 kcal</Text>
-            </ListItem>
-          </List>
-        </Box>
-        <Box w="288px">
-          <Heading
-            as="h3"
-            fontSize="14px"
-            textAlign="center"
-            justifyContent="space-between"
-            mb="20px"
-            color="#212121"
-          >
-            Food not recommended
-          </Heading>
-          <List color="#9B9FAA">
-            <ListItem display="flex" justifyContent="space-between">
-              <Text>Left</Text>
-              <Text>000 kcal</Text>
-            </ListItem>
-          </List>
-        </Box>
-      </Box>
+      <RightSideBar />
     </>
   );
 };
---
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -0,0 +1,62 @@
+import { Box, Heading, List, ListItem, Text } from '@chakra-ui/react';
+
+const RightSideBar = () => {
+  return (
+    <Box
+      bgColor="#F0F1F3"
+      minW={{ xs: '768px', md: '517px' }}
+      maxW={{ lg: '517px' }}
+      py={{ xs: '40px', md: '80px', lg: '166px' }}
+      display="flex"
+      flexDir={{ xs: 'column', md: 'row', lg: 'column' }}
+      gap={{ xs: '40px', md: '97px', lg: '60px' }}
+      justifyContent="center"
+      alignItems={{ xs: 'center', md: 'baseline', lg: 'center' }}
+      ml={{ lg: 'auto' }}
+    >
+      <Box w="288px">
+        <Heading fontSize="14px" textAlign="center" mb="20px" color="#212121">
+          Summary for 06/20/2020
+        </Heading>
+        <List color="#9B9FAA">
+          <ListItem display="flex" justifyContent="space-between">
+            <Text>Left</Text>
+            <Text>000 kcal</Text>
+          </ListItem>
+          <ListItem display="flex" justifyContent="space-between">
+            <Text>Consumed</Text>
+            <Text>000 kcal</Text>
+          </ListItem>
+          <ListItem display="flex" justifyContent="space-between">
+            <Text>Daily rate</Text>
+            <Text>000 kcal</Text>
+          </ListItem>
+          <ListItem display="flex" justifyContent="space-between">
+            <Text>n% of normal</Text>
+            <Text>000 kcal</Text>
+          </ListItem>
+        </List>
+      </Box>
+      <Box w="288px">
+        <Heading
+          as="h3"
+          fontSize="14px"
+          textAlign="center"
+          justifyContent="space-between"
+          mb="20px"
+          color="#212121"
+        >
+          Food not recommended
+        </Heading>
+        <List color="#9B9FAA">
+          <ListItem display="flex" justifyContent="space-between">
+            <Text>Left</Text>
+            <Text>000 kcal</Text>
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export default RightSideBar;
