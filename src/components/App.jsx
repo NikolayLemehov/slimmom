31a70f9fcdf3ae9973fd66eec21a40ae3199a60d
Mykola Berezhniuk <bermukola@gmail.com>
2022-11-23 11:42:46 +0200

    add container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,36 @@
-import {lazy, Suspense} from 'react';
+import { lazy, Suspense } from 'react';
+import { Box } from '@chakra-ui/react';
 import Home from 'pages/LogInPage/Home/Home';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import { LogInPage } from '../pages/LogInPage/LogInPage';
 import Header from './Header/Header';
 import CalculatorСalorieForm from './CalculatorСalorieForm/CalculatorСalorieForm';
 
-
 const UiKit = lazy(() => import('./UiKit/UiKit'));
 
 export const App = () => {
   return (
-    <>
+    <Box
+      mr="auto"
+      ml="auto"
+      pr={{ xs: '20px', md: '32px', lg: '16px' }}
+      pl={{ xs: '20px', md: '32px', lg: '16px' }}
+      maxW={{ sm: '480px', md: '798px', lg: '1280px' }}
+    >
       <Header />
       <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Outlet />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<LogInPage />} />
-          <Route path="registration" element={<div>Registration</div>} />
-          <Route path="calculator" element={<CalculatorСalorieForm />} />
-          <Route path="diary" element={<div>Diary</div>} />
-          <Route path="uikit" element={<UiKit/>}/>
-        </Route>
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Outlet />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<LogInPage />} />
+            <Route path="registration" element={<div>Registration</div>} />
+            <Route path="calculator" element={<CalculatorСalorieForm />} />
+            <Route path="diary" element={<div>Diary</div>} />
+            <Route path="uikit" element={<UiKit />} />
+          </Route>
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
       </Suspense>
-    </>
+    </Box>
   );
 };
--- src/components/CalculatorСalorieForm/CalculatorСalorieForm.styled.jsx
+++ src/components/CalculatorСalorieForm/CalculatorСalorieForm.styled.jsx
@@ -8,3 +8,17 @@ export const Form = styled.form`
 
   width: 512px;
 `;
+
+export const Title = styled.h1`
+  width: 608px;
+  margin-top: 140px;
+  margin-bottom: 48px;
+
+  font-family: 'Verdana';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 1.2;
+
+  color: #212121;
+`;
