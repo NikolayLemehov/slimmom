33cddb564b0b2ddc990acd0a847fd081c4fd5a93
Victor Grossu <weekgrossu@gmail.com>
2022-11-24 16:59:01 +0200

    Hambrurger done

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
-import { Box, Flex, IconButton, Link } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Link,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+} from '@chakra-ui/react';
+import textLogo from '../assets/logo/textLogo.svg';
+import UserMenu from 'components/UserMenu/UserMenu';
 
 const HamburgerMenu = () => {
-  const [display, changeDisplay] = useState('none');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex>
-      {display === 'none' && (
+      {!isOpen && (
         <Flex pos="fixed" top="23px" right={{ xs: '15px', md: '25px' }}>
           <IconButton
             bgColor="transparent"
@@ -16,66 +28,80 @@ const HamburgerMenu = () => {
             size="sm"
             icon={<HamburgerIcon />}
             display={{ lg: 'none' }}
-            onClick={() => changeDisplay('flex')}
+            onClick={() => {
+              onOpen();
+            }}
           />
         </Flex>
       )}
-      <Flex
-        w="100vw"
-        bgColor="#264061"
-        zIndex={20}
-        h="100vh"
-        position="absolute"
-        top="78px"
-        left="0"
-        overflowY="auto"
-        flexDir="column"
-        display={display}
-      >
-        <Flex pos="fixed" top="23px" right={{ xs: '15px', md: '25px' }}>
-          <IconButton
-            aria-label="Close Button"
-            bgColor="transparent"
-            size="sm"
-            icon={<CloseIcon />}
-            onClick={() => changeDisplay('none')}
-          />
-        </Flex>
-        <Flex
-          h="100vh"
-          alignItems="center"
-          textAlign="center"
-          flexDir="column"
-          gap="5"
-          mt={{ xs: '60px', md: '100px' }}
-        >
-          <Box>
-            <Link
-              _hover={{ textDecor: 'none', color: '#9B9FAA' }}
-              fontFamily="-moz-initial"
-              fontSize="18px"
-              as={NavLink}
-              to="/calculator"
-              mr="16px"
-              color="#FFFFFF"
+      <Modal isOpen={isOpen} onClose={onClose} size="full" motionPreset="none">
+        <ModalContent>
+          <ModalHeader
+            py="16px"
+            ml="8px"
+            fontWeight="normal"
+            fontSize="16px"
+            borderBottom="2px solid #E0E0E0"
+            justifyContent="space-between"
+            display="flex"
+            alignItems="center"
+          >
+            <NavLink to="/">
+              <img src={textLogo} alt="logo" />
+            </NavLink>
+            <Flex>
+              <Box display={{ xs: 'none', md: 'flex' }}>
+                <UserMenu />
+              </Box>
+              <IconButton
+                aria-label="Close Button"
+                bgColor="transparent"
+                size="sm"
+                icon={<CloseIcon />}
+                onClick={() => {
+                  onClose();
+                }}
+              />
+            </Flex>
+          </ModalHeader>
+          <ModalBody bgColor="#264061">
+            <Flex
+              textAlign="center"
+              flexDir="column"
+              gap="5"
+              mt={{ xs: '60px', md: '100px' }}
             >
-              CALCULATOR
-            </Link>
-          </Box>
-          <Box>
-            <Link
-              _hover={{ textDecor: 'none', color: '#9B9FAA' }}
-              fontFamily="-moz-initial"
-              fontSize="18px"
-              as={NavLink}
-              to="/diary"
-              color="#FFFFFF"
-            >
-              DIARY
-            </Link>
-          </Box>
-        </Flex>
-      </Flex>
+              <Box>
+                <Link
+                  _hover={{ textDecor: 'none', color: '#9B9FAA' }}
+                  fontFamily="-moz-initial"
+                  fontSize="18px"
+                  as={NavLink}
+                  to="/calculator"
+                  mr="16px"
+                  color="#FFFFFF"
+                  onClick={onClose}
+                >
+                  CALCULATOR
+                </Link>
+              </Box>
+              <Box>
+                <Link
+                  _hover={{ textDecor: 'none', color: '#9B9FAA' }}
+                  fontFamily="-moz-initial"
+                  fontSize="18px"
+                  as={NavLink}
+                  to="/diary"
+                  color="#FFFFFF"
+                  onClick={onClose}
+                >
+                  DIARY
+                </Link>
+              </Box>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
