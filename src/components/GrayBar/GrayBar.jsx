6d872482bac19131d856dbba00f7727fc8a05e9d
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-11-25 06:27:30 +0200

    add responsive login & registration

--- src/components/GrayBar/GrayBar.jsx
+++ src/components/GrayBar/GrayBar.jsx
@@ -3,10 +3,15 @@ import UserMenu from 'components/UserMenu/UserMenu';
 import { IoReturnDownBackSharp } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
 import { authSelectors } from 'redux/auth/authSelectors';
+import {useLocation} from "react-router-dom";
+import {useMemo} from "react";
 
 const GrayBar = ({ onClick }) => {
+  const location = useLocation();
+  const isVisible = useMemo(() => !['/login', '/registration'].includes(location.pathname), [location])
+
   const isLogin = useSelector(authSelectors.isLoggedIn);
-  return (
+  return isVisible && (
     <Box display={{ md: 'none' }} w="100%">
       <Flex
         bgColor="#EFF1F3"
