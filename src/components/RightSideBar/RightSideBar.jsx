c42b2b86e7a9df1fa06cd3973ca21c0645f9b0d8
Mykola Berezhniuk <bermukola@gmail.com>
2022-11-25 19:20:46 +0200

    add logig for calculator

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -5,18 +5,11 @@ const RightSideBar = () => {
     <Flex
       bgColor="#F0F1F3"
       h="100%"
-      // w={{ xs: '100%', lg: '617px' }}
       py={{ xs: '40px', md: '80px', lg: '0px' }}
       flexDir={{ xs: 'column', md: 'row', lg: 'column' }}
       gap={{ xs: '40px', md: '97px', lg: '60px' }}
       justifyContent={{ xs: 'center', lg: 'start' }}
       alignItems={{ xs: 'center', md: 'baseline', lg: 'center' }}
-      // position={{ lg: 'fixed' }}
-      // top={{ lg: '0' }}
-      // right={{ lg: '0' }}
-      // bgImage={{ md: leafMd, lg: leafLg }}
-      // bgPos="right"
-      // bgRepeat="no-repeat"
     >
       <Box w="288px">
         <Heading fontSize="14px" textAlign="center" mb="20px" color="#212121">
